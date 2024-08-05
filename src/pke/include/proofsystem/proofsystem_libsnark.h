58cb702bace61a79cb1e3501c670bc9bd41c3f60
graikos <raikosgiorgos7@gmail.com>
2024-08-05 18:16:10 +0300

    update fixes

--- src/pke/include/proofsystem/proofsystem_libsnark.h
+++ src/pke/include/proofsystem/proofsystem_libsnark.h
@@ -2,7 +2,7 @@
 #define OPENFHE_PROOFSYSTEM_LIBSNARK_H
 
 #include "proofsystem.h"
-#include "libff/algebra/fields/field_utils.hpp"
+#include "libff/algebra/field_utils/field_utils.hpp"
 #include "libsnark/zk_proof_systems/ppzksnark/r1cs_ppzksnark/r1cs_ppzksnark.hpp"
 #include "libsnark/common/default_types/r1cs_ppzksnark_pp.hpp"
 #include "libsnark/gadgetlib1/pb_variable.hpp"
